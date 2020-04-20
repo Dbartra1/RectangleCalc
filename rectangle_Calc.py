27a93b93087a383256fe4f174cd8422e1b3442d9
Dbartra1 <53958057+Dbartra1@users.noreply.github.com>
2020-04-19 22:33:32 -0600

    Update rectangle_Calc.py
    final update

--- rectangle_Calc.py
+++ rectangle_Calc.py
@@ -13,31 +13,55 @@ class Rectangle:
     
     def printRectangle (self):
         output = ""
-        for i in range(1, self.width+1):
-            for j in range(1, self.height+1):
-                if(i == 1 or i == self.height or
-                  j == 1 or j == self.width):                    
-                    output += "*"
-                    print("*", end = " ")
+        for row in range(0, self.height):
+            for column in range(0, self.width):
+                if (row == 0 or row == self.height - 1 or column == 0 or column == self.width - 1):
+                    output += "* "
                 else:
-                    print(" ", end=" ")
-            print()
+                    output += "  "
+            output += "\n"
         return output
 
+class Square(Rectangle):
+    def __init__(self, width, height):
+       
+        super(Square, self).__init__(width, height)
+        
+
 def main():
-    print ("Rectangle Calculator\n")
-    while True:
-        a = int(input("Height: "))
-        b = int(input("Width: "))
-        obj = Rectangle(a,b)
-        print("Area: ", obj.getArea())
-        print("Peremeter: ", obj.getPeremeter())
-        print()
-        obj.printRectangle()
-        choice = input("\nContinue? (y/n): ")
-        if choice != "y":
-            print ("\nBye!")
+    print("--RECTANGLE CALCULATOR--")
+    choice1 = 'y'
+    while choice1.lower() == 'y':
+        while True:
+            choice2 = str(input("Rectangle or Square? (s/r): "))
+            if choice2.lower() == 's':
+                print ("Im a Square!!")
+                a = int(input("Length: "))
+                obj = Square(a,a)
+                print("Area: ", obj.getArea())
+                print("Peremeter: ", obj.getPeremeter())
+                print()
+                print(obj.printRectangle())
+                break
+            if choice2.lower() == 'r':
+                print ("Im a Rectangle")
+                a = int(input("Height: "))
+                b = int(input("Width: "))
+                obj = Rectangle(a,b)
+                print("Area: ", obj.getArea())
+                print("Peremeter: ", obj.getPeremeter())
+                print()
+                print(obj.printRectangle())
+                break
+            else:
+                print ("Please Make a Proper Selection")
+                continue
+        choice1 = str(input("Would you like to continue? (y/n): "))
+        if choice1.lower() != 'y':
+            print("BYE!!!!")
             break
-            
+
+
 if __name__ == "__main__":
     main()
+
